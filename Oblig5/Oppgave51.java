a6f9d09acb9e93399dd1a19c961aba1354accc8d
yastaheran <y.taheranpour@gmail.com>
2017-06-09 21:07:06 +0200

    Create Oppgave51.java

---
+++ Oblig5/Oppgave51.java
@@ -0,0 +1,10 @@
+public class Oppgave51 {
+	public static void main(String[] args) {
+		Person nyPerson = new Person("Yas");
+		nyPerson.setAlder(19);
+		nyPerson.setBosted("Ski");
+		Mobil nyMobil = new Mobil("iPhone");
+		nyMobil.setEier("Yas");
+		nyMobil.setTlf(45887257);	
+	}
+}

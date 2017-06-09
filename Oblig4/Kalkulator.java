4de5ed3aad6e8df6994e9b30859a39c9fe9a3434
yastaheran <y.taheranpour@gmail.com>
2017-06-09 21:04:05 +0200

    Create Kalkulator.java

---
+++ Oblig4/Kalkulator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Kalkulator {
+	public static void main(String[] args) {
+		int addSvar = addisjon(3, 4);
+		System.out.println(addSvar);
+		int subSvar = subtraksjon(5, 2);
+		System.out.println(subSvar);
+		int helDivSvar = heltallsdivisjon(10, 3);
+		System.out.println(helDivSvar);
+		double divSvar = divisjon(10, 3);
+	}
+//Oppgave A - addisjon
+	public static int addisjon(int heltall1, int heltall2) {
+		Scanner plussInn = new Scanner(System.in);
+		int sum = heltall1 + heltall2;
+		return sum;
+	}
+//Oppgave B - subtraksjon
+	public static int subtraksjon(int heltall1, int heltall2) {
+		Scanner minusInn = new Scanner(System.in);
+		int sum = heltall1 - heltall2;
+		return sum;
+	}
+//Oppgave C - heltallsdivisjon
+	public static int heltallsdivisjon(int heltall1, int heltall2) {
+		Scanner heltallDivInn = new Scanner(System.in);
+		int sum = heltall1/heltall2;
+		return sum;
+	}
+//Oppgave D - divisjon med desimaler
+	public static double divisjon(double tall1, double tall2) {
+		Scanner divInn = new Scanner(System.in);
+		double sum = tall1/tall2;
+		return sum;
+	}
+}

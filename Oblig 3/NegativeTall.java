1c0613d0dbad516935f03b42c2e3f8ec86a40806
yastaheran <y.taheranpour@gmail.com>
2017-06-09 21:00:15 +0200

    Create NegativeTall.java

---
+++ Oblig 3/NegativeTall.java
@@ -0,0 +1,29 @@
+public class NegativeTall {
+	public static void main(String[] args) {
+		int[] arrayTall = {1,4,5,-2,-4,6,10,3,-2};
+		int antallNeg = 0;
+		int sumAntall = 0;		
+//Oppgave A
+		for(int i = 0; i < arrayTall.length; i++){
+			if (arrayTall[i] < 0){
+				antallNeg = 1;
+				sumAntall += antallNeg;
+			}
+		}
+		System.out.println("det er " + sumAntall + " negativetall i arrayen");
+//Oppgave B
+		int i = 0;
+		while(arrayTall.length < 0) {
+			antallNeg =1;
+			sumAntall += antallNeg;
+			i ++;
+		}
+		System.out.println("det er " + sumAntall + " negativetall i arrayen");
+//Oppgave C
+		for (int i = 0; i < arrayTall.length; i++)  {
+			if (arrayTall|[i] < 0)  {
+				arrayTall[i] = i;
+			}
+		}	
+	}
+}

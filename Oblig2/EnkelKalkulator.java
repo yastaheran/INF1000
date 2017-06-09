2452d39b647bf49e2e8f3a2770f6aee9b71b0130
yastaheran <y.taheranpour@gmail.com>
2017-06-09 22:04:33 +0200

    Create EnkelKalkulator.java

--- Oblig2/EnkelKalkulator.java
+++ Oblig2/EnkelKalkulator.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 public class EnkelKalkulator {
 	public static void main(String[] args) {
 		Scanner tallIn = new Scanner(System.in);
+		System.out.println("Skriv inn et heltall:");
+		int heltall1 = tallIn.extInt();
+		System.out.println("Skriv inn et annet heltall:");
+		int heltall2 = tallIn.extInt();
 //Oppgave A
-		sumMetode();			
+		sumMetode(heltall1, helltall2);	
 //Oppgave B
-		difMetode();
+		difMetode(heltall1, helltall2);
 //Oppgave C
-		prodMetode();
+		prodMetode(heltall1, helltall2);
 //Oppgave D
 		System.out.println("Skriv inn et heltall:");
 		int heltall1 = tallIn.nextInt();
@@ -15,28 +19,13 @@ public class EnkelKalkulator {
 		int heltall2 = tallIn.nextInt();
 		System.out.println("Summen er lik " + (heltall1 + heltall2) + "Differansen er lik " + (heltall1 - 		heltall2) + "Produktet er lik " + (heltall1 * heltall2));
 	}
-	public static void sumMetode() {
-		Scanner tallIn = new Scanner(System.in);
-		System.out.println("Skriv inn et heltall:");
-		int heltall1 = tallIn.nextInt();
-		System.out.println("Skriv inn et annet heltall:");
-		int heltall2 = tallIn.nextInt();
-		System.out.println("Summen er lik " + (heltall1 + heltall2));	
+	public static void sumMetode(int t1, int t2) {
+		System.out.println("Summen er lik " + (t1 + t2));	
 	}
-	public static void difMetode() {
-		Scanner tallIn = new Scanner(System.in);
-		System.out.println("Skriv inn et heltall:");
-		int heltall1 = tallIn.nextInt();
-		System.out.println("Skriv inn et annet heltall:");
-		int heltall2 = tallIn.nextInt();
+	public static void difMetode(int t1, int t2) {
 		System.out.println("Summen er lik " + (heltall1 - heltall2));	
 	}
-	public static void prodMetode() {
-		Scanner tallIn = new Scanner(System.in);
-		System.out.println("Skriv inn et heltall:");
-		int heltall1 = tallIn.nextInt();
-		System.out.println("Skriv inn et annet heltall:");
-		int heltall2 = tallIn.nextInt();
-		System.out.println("Produktet er lik " + (heltall1 * heltall2));
+	public static void prodMetode(int t1, int t2) {
+		System.out.println("Produktet er lik " + (t1 * t2));
 	}
 }

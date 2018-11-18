f063b1d9184e6a1b60df304e51a4acae0582bcfd
yastaheran <y.taheranpour@gmail.com>
2018-11-18 15:34:41 +0100

    Create TicTacToeGrid.java

---
+++ Ukesoppgaver/1/TicTacToeGrid.java
@@ -0,0 +1,12 @@
+public class TicTacToeGrid {			//et program som lager et tre-paa-rad-brett
+	public static void main (String[] args) {
+		String deling1 = "+--+--+--+";
+		String deling2 = "|  |  |  |";
+		System.out.println(deling1);
+		Syste.out.printlln(deling2);
+		System.out.println(deling1);
+		Syste.out.printlln(deling2);
+		System.out.println(deling1);
+		Syste.out.printlln(deling2);
+	}
+}

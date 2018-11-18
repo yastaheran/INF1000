bd08b984eb1060d4523b5744618f1c8317fb00b3
yastaheran <y.taheranpour@gmail.com>
2018-11-18 15:36:52 +0100

    Create SjekkFortegn.java

---
+++ Ukesoppgaver/1/SjekkFortegn.java
@@ -0,0 +1,28 @@
+public class SjekkFortegn{			//programmet skal sjekke om tallet er negativt/positivt eller 0
+	public stativ void main(String[] args) {
+		int var1 = 0;
+		int var2 = ;
+		int var3 = ;
+		if(var1 == 0) {
+			System.out.println("var1 er lik 0);
+		} else if(var1 < 0) {
+			System.out.println("var1 er positiv);
+		} else {
+			System.out.println("var1 er negativ);
+		}
+		if(var2 == 0) {
+			System.out.println("var2 er lik 0);
+		} else if(var2 < 0) {
+			System.out.println("var2 er positiv);
+		} else {
+			System.out.println("var2 er negativ);
+		}
+		if(var3 == 0) {
+			System.out.println("var3 er lik 0);
+		} else if(var3 < 0) {
+			System.out.println("var3 er positiv);
+		} else {
+			System.out.println("var3 er negativ);
+		}
+	}
+}

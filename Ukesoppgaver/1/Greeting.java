cafdd15ffe7b0b058f99fb2e382f7982f906ed67
yastaheran <y.taheranpour@gmail.com>
2018-11-18 15:34:18 +0100

    Create Greeting.java

---
+++ Ukesoppgaver/1/Greeting.java
@@ -0,0 +1,8 @@
+public class Greeting {		//et program som skriver ut en hilsning på flere språk
+	public static void main (String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hei Verden!");
+		System.out.println("Bonjour le monde!");
+		System.out.println("Hola mundo");
+	}
+}
